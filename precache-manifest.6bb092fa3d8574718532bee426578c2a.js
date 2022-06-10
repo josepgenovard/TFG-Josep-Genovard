a2c3a5e5accb534985d311f01fbb250e151f24ab
Josep Genovard Oliver <josepgenovard@gmail.com>
2022-06-10 10:14:35 +0200

    Updates

---
+++ precache-manifest.6bb092fa3d8574718532bee426578c2a.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf1c990e30e9a3456fb0aba51ab57241",
+    "url": "/EthereumReactJSTemplate/index.html"
+  },
+  {
+    "revision": "80643654fad87a1b02e7",
+    "url": "/EthereumReactJSTemplate/static/css/2.ace9d170.chunk.css"
+  },
+  {
+    "revision": "80643654fad87a1b02e7",
+    "url": "/EthereumReactJSTemplate/static/js/2.16943587.chunk.js"
+  },
+  {
+    "revision": "f77fae95abae31683cb99e06e96d85d4",
+    "url": "/EthereumReactJSTemplate/static/js/2.16943587.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a2089ece11bdde55d24",
+    "url": "/EthereumReactJSTemplate/static/js/main.f3221201.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/EthereumReactJSTemplate/static/js/main.f3221201.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e2891822d7767267bab",
+    "url": "/EthereumReactJSTemplate/static/js/runtime-main.9f6dbd84.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/EthereumReactJSTemplate/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/EthereumReactJSTemplate/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/EthereumReactJSTemplate/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/EthereumReactJSTemplate/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/EthereumReactJSTemplate/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/EthereumReactJSTemplate/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/EthereumReactJSTemplate/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/EthereumReactJSTemplate/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/EthereumReactJSTemplate/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/EthereumReactJSTemplate/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/EthereumReactJSTemplate/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/EthereumReactJSTemplate/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/EthereumReactJSTemplate/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/EthereumReactJSTemplate/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/EthereumReactJSTemplate/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/EthereumReactJSTemplate/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
