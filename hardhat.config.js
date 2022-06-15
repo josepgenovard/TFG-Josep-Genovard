7cc8da010be290085efb135b982ce0c37b40e85c
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-06-15 11:51:43 +0200

    compile with hardhat 0.8.7

--- hardhat.config.js
+++ hardhat.config.js
@@ -67,7 +67,7 @@ module.exports = {
   solidity: {
     compilers: [
       {
-        version: '0.7.4',
+        version: '0.8.7',
         settings: { optimizer: { enabled: true, runs: 200 } },
       },
     ],
